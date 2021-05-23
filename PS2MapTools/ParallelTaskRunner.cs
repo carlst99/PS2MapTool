9f49e397e3d8ba2829a788fe9f5a70d2f23d340a
Carl Stephens <carlst99@outlook.co.nz>
2021-05-24 11:51:32 +1200

    Improve ParallelTaskRunner

--- PS2MapTools/ParallelTaskRunner.cs
+++ PS2MapTools/ParallelTaskRunner.cs
@@ -7,13 +7,16 @@ using System.Threading.Tasks;
 namespace PS2MapTools
 {
     /// <summary>
-    /// 
+    /// Runs multiple tasks in parallel, with support for a parallelism limit.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class TaskRunner<T> : IDisposable
+    public class ParallelTaskRunner : IDisposable
     {
-        //Action<IConsole, CancellationToken>
-        protected readonly ConcurrentQueue<Action<T>> _taskQueue;
+        /// <summary>
+        /// The delay in milliseoncds that waiting threads will pause for.
+        /// </summary>
+        private const int WAIT_DELAY = 100;
+
+        protected readonly ConcurrentQueue<Task> _taskQueue;
         protected readonly List<Task> _runningTasks;
 
         /// <summary>
@@ -43,22 +46,39 @@ namespace PS2MapTools
         /// </summary>
         public bool IsDisposed { get; private set; }
 
-        public TaskRunner()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParallelTaskRunner"/> object.
+        /// </summary>
+        public ParallelTaskRunner()
         {
-            _taskQueue = new ConcurrentQueue<Action<T>>();
+            _taskQueue = new ConcurrentQueue<Task>();
             _runningTasks = new List<Task>();
             _taskRunner = new Task(() => throw new InvalidOperationException("This task runner has not been setup."));
-
-            if (typeof(T).IsSubclassOf(typeof(Action)))
-                throw new ArgumentException("Generic type must derive from System.Action");
         }
 
-        public virtual void EnqueueTask(Action<T> action) => _taskQueue.Enqueue(action); // Just accept tasks?
+        /// <summary>
+        /// Adds a tasks to the processing queue.
+        /// </summary>
+        /// <param name="task">The task to run.</param>
+        public virtual void EnqueueTask(Task task)
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(ParallelTaskRunner));
 
-        public virtual void Start(T data, CancellationToken ct, int maxParallelTasks = 4)
+            _taskQueue.Enqueue(task);
+        }
+
+        /// <summary>
+        /// Begins running tasks.
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <param name="maxParallelTasks">The maximum number of tasks that may be run in parallel.</param>
+        public virtual void Start(CancellationToken ct, int maxParallelTasks = 4)
         {
             if (IsRunning)
                 throw new InvalidOperationException("This instance is already running.");
+            if (IsDisposed)
+                throw new ObjectDisposedException(nameof(ParallelTaskRunner));
 
             ct.Register(() => IsRunning = false);
 
@@ -66,22 +86,27 @@ namespace PS2MapTools
             {
                 while (IsRunning && !ct.IsCancellationRequested)
                 {
-                    while (_runningTaskCount < maxParallelTasks && _taskQueue.TryDequeue(out Action<T>? a))
+                    while (_runningTaskCount < maxParallelTasks && _taskQueue.TryDequeue(out Task? t))
                     {
-                        Task t = new(() => a.Invoke(data), TaskCreationOptions.LongRunning);
                         t.ContinueWith((t) =>
                         {
+                            Monitor.Enter(_runningTasks);
                             _runningTasks.Remove(t);
+                            Monitor.Exit(_runningTasks);
+
                             Interlocked.Decrement(ref _runningTaskCount);
                         });
-                        _runningTasks.Add(t);
-                        t.Start();
 
                         Interlocked.Increment(ref _runningTaskCount);
-                        // Cache tasks and wait on them completing before exiting
+
+                        Monitor.Enter(_runningTasks);
+                        _runningTasks.Add(t);
+                        Monitor.Exit(_runningTasks);
+
+                        t.Start();
                     }
 
-                    Task.Delay(100).Wait();
+                    Task.Delay(WAIT_DELAY).Wait();
                 }
             }, ct, TaskCreationOptions.LongRunning);
 
@@ -89,18 +114,23 @@ namespace PS2MapTools
         }
 
         /// <summary>
-        /// Stops this <see cref="TaskRunner{T}"/> instance. Any queued tasks will not be processed, and currently active tasks will be finished.
+        /// Clears any queued tasks that are yet to be run.
+        /// </summary>
+        public void ClearTaskQueue() => _taskQueue.Clear();
+
+        /// <summary>
+        /// Stops this <see cref="TaskRunner{T}"/> instance. Currently active tasks will be finished.
         /// </summary>
         public virtual void Stop() => IsRunning = false;
 
         /// <summary>
-        /// Waits for all tasks (active + queued) to complete.
+        /// Waits for all remaining tasks to complete.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A <see cref="Task"/> that represents the wait operation.</returns>
         public virtual async Task WaitForAll()
         {
-            while (_runningTaskCount != 0 && !_taskQueue.IsEmpty)
-                await Task.Delay(100).ConfigureAwait(false);
+            while (_runningTaskCount != 0 || (!_taskQueue.IsEmpty && IsRunning))
+                await Task.Delay(WAIT_DELAY).ConfigureAwait(false);
         }
 
         public void Dispose()
@@ -111,6 +141,9 @@ namespace PS2MapTools
 
         protected virtual void Dispose(bool disposing)
         {
+            if (_runningTaskCount != 0)
+                throw new InvalidOperationException("Ensure that this " + nameof(ParallelTaskRunner) + " is stopped, and all remaining tasks have been processed, before disposing of it.");
+
             if (!IsDisposed)
             {
                 if (disposing)
