7a5cf9f24ef60f3d3d32c9c89a5a37957f5adc51
Carl Stephens <carlst99@outlook.co.nz>
2021-05-25 10:43:35 +1200

    Fix issues with spawning OptiPNG process

--- PS2MapTools/Services/CompressionService.cs
+++ PS2MapTools/Services/CompressionService.cs
@@ -32,32 +32,34 @@ namespace PS2MapTools.Services
             Task compressionTask = new(() =>
             {
                 _console.MarkupLine($"{ PREFIX }Beginning compression on { Formatter.Path(Path.GetFileName(filePath)) }");
+
                 try
                 {
                     Process? process = Process.Start(new ProcessStartInfo(OPTIPNG_FILE_NAME, filePath)
                     {
                         CreateNoWindow = true,
-                        RedirectStandardOutput = true
+                        RedirectStandardError = true
                     });
 
                     if (process is null)
                         return;
 
-                    // Fixes issues with OptiPNG spawning a child process
-                    process.BeginOutputReadLine();
+                    // OptiPNG outputs to stderr, and also seems to use a 'starter process', resulting in Process.HasExited being set initially. Waiting for output solves this.
                     bool canWait = false;
-                    process.OutputDataReceived += (_, __) => canWait = true;
+                    process.BeginErrorReadLine();
+                    process.ErrorDataReceived += (_, __) => canWait = true;
                     while (!canWait)
                         Task.Delay(100).Wait();
 
                     process.WaitForExit();
                     process.Dispose();
-                    _console.MarkupLine($"{ PREFIX }{ Formatter.Success("Completed") } compressing { Formatter.Path(Path.GetFileName(filePath)) }");
                 }
                 catch (Exception ex)
                 {
                     _console.MarkupLine($"{ PREFIX }{ Formatter.Error("Compression failed:") } {ex}");
                 }
+
+                _console.MarkupLine($"{ PREFIX }{ Formatter.Success("Completed") } compressing { Formatter.Path(Path.GetFileName(filePath)) }");
             }, ct, TaskCreationOptions.LongRunning);
 
             _taskRunner.EnqueueTask(compressionTask);
