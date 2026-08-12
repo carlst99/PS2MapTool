a33e280b1a0f5f57f8edb44a8ddbbc1028b42f11
Carl <carlst99@outlook.co.nz>
2026-08-13 10:46:49 +1200

    build: update publish script

--- publish.ps1
+++ publish.ps1
@@ -7,8 +7,8 @@ function New-MapToolArchive
         $ArchiveName
     )
 
-    $archiveOutput = ".\bin\publish\" + $ArchiveName
-    Get-ChildItem -Path ".\bin\publish\raw" | Compress-Archive -DestinationPath $archiveOutput -Force
+    $archiveOutput = ".\bin\Publish\" + $ArchiveName
+    Get-ChildItem -Path ".\bin\Publish\raw" | Compress-Archive -DestinationPath $archiveOutput -Force
     if (-not $?)
     {
         Write-Error "Failed to zip publish files."
@@ -19,22 +19,13 @@ function New-MapToolArchive
 $currDir = Get-location
 Set-Location ".\PS2MapTool.Cli"
 
-dotnet publish -o ".\bin\publish\raw" --no-self-contained -r win-x64 -p:PublishSingleFile=true -c Release
-if (-not $?)
-{
-    Write-Error "Failed to publish framework-dependent executable."
-    exit
-}
-
-New-MapToolArchive "PS2MapTool_Framework-Dependent.zip"
-
-dotnet publish -o ".\bin\publish\raw" --self-contained -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -c Release
+dotnet publish -o ".\bin\Publish\raw" --self-contained -r win-x64 -p:PublishSingleFile=true -p:PublishAot=true -c Release
 if (-not $?)
 {
     Write-Error "Failed to publish self-contained executable."
     exit
 }
 
-New-MapToolArchive "PS2MapTool_Self-Contained.zip"
+New-MapToolArchive "PS2MapTool.zip"
 
 Set-Location $currDir
